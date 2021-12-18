5b82a62f5ca9552c19804ceb054a5c53214514a1
Thiago Yamanaka <thiagoyb@gmail.com>
2021-12-18 20:00:03 -0300

    javascript recebendo dados do forumulario e guardando em uma variavel json

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-alert('Hello world');
+let form = document.querySelector('#form-user-create');
+var user = {};
+form.querySelector('[type=submit]').addEventListener('mouseover', ()=>{
+    form.querySelectorAll('[name]').forEach((e, i)=>{
+        if(e.type=="radio"){
+            if(e.checked) user[e.name] = e.value;
+        } else{
+            if(e.value!='') user[e.name] = e.value;
+            /*else{
+                alert('Preeencha o campo '+e.name);
+                e.focus();
+                return;
+            }*/
+        }
+    });
+    return false;
+});
+
+form.addEventListener('submit', event =>{
+    event.preventDefault();
+    console.log(user);
+});
+
