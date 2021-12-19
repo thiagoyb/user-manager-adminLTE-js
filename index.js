6fc57d4a340d088b800ad833451d001b762fc6ca
Thiago Yamanaka <thiagoyb@gmail.com>
2021-12-18 22:51:34 -0300

    Adicionado função que adiciona nova linha a tabela de usuarios

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 let form = document.querySelector('#form-user-create');
 var user = {};
-form.querySelector('[type=submit]').addEventListener('mouseover', ()=>{
+
+const addLine = (dataUser) =>{
+    let tr = document.createElement("TR");
+    tr.innerHTML = `
+        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td>${dataUser.name}</td>
+        <td>${dataUser.email}</td>
+        <td>${dataUser.admin}</td>
+        <td>${dataUser.birth}</td>
+        <td>
+            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>`;
+        document.getElementById('tableUsers').appendChild(tr);
+}
+
+form.addEventListener('submit', event =>{
+    event.preventDefault();
     form.querySelectorAll('[name]').forEach((e, i)=>{
         if(e.type=="radio"){
             if(e.checked) user[e.name] = e.value;
@@ -13,11 +30,6 @@ form.querySelector('[type=submit]').addEventListener('mouseover', ()=>{
             }*/
         }
     });
-    return false;
-});
-
-form.addEventListener('submit', event =>{
-    event.preventDefault();
-    console.log(user);
+    addLine(user);
 });
 
